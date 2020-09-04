8a1ef57dc0de1fa0ecbba36302c93eb6d13bad68
Diogo Torres <diogo.torres93@gmail.com>
2020-09-03 23:10:24 -0300

    estilizando o tema

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,14 @@ class ByteBankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.green[900],
+        accentColor: Colors.blueAccent[700],
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.blueAccent[700],
+          textTheme: ButtonTextTheme.primary
+        )
+      ),
       home: ListaTransferencia(),
     );
   }
